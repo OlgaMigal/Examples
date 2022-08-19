428cb3af939e78fc45ace235deaafb70d0d3fed4
Olga_Migal <Tashaloriya@gmail.com>
2022-08-20 01:28:19 +0300

    Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="асгард")
+{
+Console.WriteLine("Ура, это же красотулечка!!!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
