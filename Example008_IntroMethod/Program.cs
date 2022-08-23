60b9971d50afe35aa010a065e9f19b9236ec2153
Olga_Migal <Tashaloriya@gmail.com>
2022-08-23 19:49:49 +0300

    Example008 IntroMethod

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти максимум из 9 заданных чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
+int a1 = 5;
+int b1 = 76;
+int c1 = 92;
+int a2 = 8;
+int b2 = 11;
+int c2 = 102;
+int a3 = 547;
+int b3 = 14;
+int c3 = 1;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
---
+++ Seminar2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать 
+//на вход два числа и выводить, является ли второе число кратным первому.
+//Если второе число не кратно числу первому, то програм
+Console.WriteLine("Введите первое число: ");
+number1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+number2 = Console.ReadLine();
+if (number2 % number1 == 0)
+{
+Console.WriteLine("Число {number2} кратно числу {number1}");
+}
+else Console.WriteLine("Число {number2} не кратно числу {number1}");
